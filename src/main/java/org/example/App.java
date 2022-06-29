9d70992ccfad8cb3bf3c1fd6f9a6a16feffb5971
myGit <528648093@qq.com>
2022-06-29 23:19:08 +0800

    new-02

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,11 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("t");
     }
 }
